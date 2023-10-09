614e2b91b89b687c6c845a0e8d0dc7777941dcc2
ricardo <ricardo@DESKTOP-JGTJSPF>
2023-10-08 21:11:47 -0300

    Add test project and correct project location

--- Askme.Questions.Api/Program.cs
+++ Askme.Questions.Api/Program.cs
@@ -1,3 +1,5 @@
+using Askme.Questions.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// todo: export to startup.cs
+builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+builder.Services.AddScoped<IQuestionListRepository, QuestionListRepository>();
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
 
 var app = builder.Build();
 
